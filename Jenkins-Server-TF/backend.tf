80694c912a2cf3a327f12d89937470c93a9cde89
saitheja <saithejas27@gmail.com>
2026-03-10 14:20:13 +0530

    updated terrafrom backend

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -2,11 +2,13 @@ terraform {
   backend "s3" {
     bucket       = "ramyasree-devops-tf-state"
     region       = "ap-south-1"
-    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     encrypt      = true
     use_lockfile = true
   }
+
   required_version = ">=1.13.3"
+
   required_providers {
     aws = {
       version = ">= 6.23.0"
@@ -14,3 +16,4 @@ terraform {
     }
   }
 }
+
