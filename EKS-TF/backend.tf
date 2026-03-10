4397c87bc330485cc102662bb105c60fd0af58e3
saitheja <saithejas27@gmail.com>
2026-03-10 15:04:50 +0530

    updated backend.tf for eks creation with  jenkins

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,15 +1,17 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
-    region       = "us-east-1"
+    bucket       = "ramyasree-devops-tf-state"
+    region       = "ap-south-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    use_lockfile = true
     encrypt      = true
+    use_lockfile = true
   }
-  required_version = ">=1.14.0"
+
+  required_version = ">=1.13.3"
+
   required_providers {
     aws = {
-      version = ">= 5.49.0"
+      version = ">= 6.23.0"
       source  = "hashicorp/aws"
     }
   }
